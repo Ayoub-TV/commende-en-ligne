00eafebd98deb492b3610d74e708c0ea85b119a1
Ayoub-TV <ayou.labouz@gmail.com>
2026-07-21 17:07:18 +0100

    Create login.js

---
+++ login.js
@@ -0,0 +1,40 @@
+import { app } from "./firebase-config.js";
+
+import {
+    getAuth,
+    signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
+
+const auth = getAuth(app);
+
+const form = document.getElementById("loginForm");
+const error = document.getElementById("error");
+
+form.addEventListener("submit", async (e) => {
+
+    e.preventDefault();
+
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value;
+
+    try {
+
+        await signInWithEmailAndPassword(auth, email, password);
+
+        if (email === "admin@test.com") {
+
+            location.href = "admin.html";
+
+        } else if (email === "kitchen@test.com") {
+
+            location.href = "kitchen.html";
+
+        }
+
+    } catch (err) {
+
+        error.textContent = "Email ou mot de passe incorrect.";
+
+    }
+
+});
